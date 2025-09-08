214de6ec35ceb446687686add2dea1c5d646ffa5
Chine Chen <chainchen1103@gmail.com>
2025-09-08 09:56:19 +0800

    perf: skip process when not thing change

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@
 //!   12.772%  refactor
 //!    4.887%  docs
 //! ```
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, Context, Ok, Result};
 use clap::{Parser, Subcommand};
 use git2::{DiffFormat, IndexAddOption, Repository};
 use ndarray::{arr2, Array2, CowArray};
@@ -44,6 +44,7 @@ use ort::value::Value;
 use ort::GraphOptimizationLevel;
 use std::collections::BTreeMap;
 use std::fs;
+use std::iter::Skip;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
@@ -147,6 +148,7 @@ fn run_commit(
 
     if diff_text_full.is_empty(){
         print!("Not thing change");
+        return Ok(());
     }
     let diff_proc = {
         let s = if only_added {
